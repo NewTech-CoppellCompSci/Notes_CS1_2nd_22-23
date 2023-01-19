2ce59b59b1a148dc0889a773ec41f60391c1df92
rshunter05 <58668710+rshunter05@users.noreply.github.com>
2023-01-19 10:41:15 -0600

    Create Notes16_String_Methods.java

---
+++ src/notes/Notes16_String_Methods.java
@@ -0,0 +1,58 @@
+package notes;
+
+/**
+ * Notes:  STRING METHODS
+ * 
+ * String are NOT data types (int, double, char...).  So,
+ * to manipulate them we must use methods.
+ * 
+ * Add the method like this
+ *     <String>.methodName(<parameters>) 
+ *
+ * 
+ * Methods:
+ * 
+ * .equals(<String>)
+ * 	 Returns 
+ * 		true if the String is EXACTLY the same as the parameter	
+ *  	false otherwise
+ * 
+ * .equalsIgnoreCase(<String>)
+ * 	 Return 
+ * 		true if the String is EXACTLY the same as the parameter
+ *           however, it does not compare upper/lower case.
+ *  	false otherwise
+ * 
+ * .length()
+ *   Return -> 
+ *     
+ *     
+ * .substring(<int>)
+ *   Return -> 
+ * 
+ * 
+ * .substring(<int>, <int>)
+ *	 Return -> 
+ *
+ *
+ */
+
+public class Notes16_String_Methods {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
